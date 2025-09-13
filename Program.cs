c0ea93c6e1a044c60ae97d0493a78e64c54906a6
Nardi Rens <nardir@axerrio.com>
2025-09-13 15:15:36 +0200

    initial commit

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+
+/*
+echo "# abs-migration-gittest" >> README.md
+git init
+git add README.md
+git commit -m "first commit"
+git branch -M main
+git remote add origin https://github.com/nardir/abs-migration-gittest.git
+git push -u origin main
+ */
